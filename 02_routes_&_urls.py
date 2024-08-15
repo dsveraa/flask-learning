cd99c0019a545804bf0aa2384c6e8c5babfc69fc
David Vera <dsvera.a@gmail.com>
2024-08-14 21:53:18 -0400

    config

---
+++ 02_routes_&_urls.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+@app.route('/hello')
+def hello():
+    response = make_response('Hello World\n')
+    response.status_code = 202
+    response.headers['content-type'] = 'text/plain'
+    return response
+
+@app.route('/getpost', methods=['GET', 'POST'])
+def getpost():
+    if request.method == 'GET':
+        return 'You made a GET request\n'
+    elif request.method == 'POST':
+        return 'You made a POST request\n'
+
+@app.route('/hello/<name>')
+def greet(name):
+    return f"Hello {name}", 201
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args['name']
+        return f'{greeting}, {name}'
+    else:
+        return 'Faltan algunos parámetros'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
---
+++ 03_templates_&_html_files.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    myvalue = 'Hello World'
+    myresult = 10 + 20
+    mylist = [10,20,30,40,50]
+    return render_template('index.html', myvalue=myvalue, myresult=myresult, mylist=mylist)
+
+@app.route('/other')
+def other():
+    some_text = ' Hello World'
+    return render_template('other.html', some_text=some_text)
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s * times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
