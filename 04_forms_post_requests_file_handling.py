879bd8d9604a29e6252328c4f29826ac0df0189a
David Vera <dsvera.a@gmail.com>
2024-08-21 13:39:36 -0400

    forms_post_requests_file_handling

--- 04_forms_post_requests_file_handling.py
+++ 04_forms_post_requests_file_handling.py
@@ -1,5 +1,8 @@
+import os
+import uuid
+import pandas
 import pandas as pd
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
 
 app = Flask(__name__, template_folder='templates')
 
@@ -16,10 +19,62 @@ def index():
         else:
             return 'Fail'
 
+
 @app.route('/file_upload', methods=['POST'])
 def file_upload():
     file = request.files['file']
 
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+        try:
+            df = pandas.read_excel(file, engine='openpyxl')
+            return df.to_html()
+        except Exception as e:
+            return str(e)
+
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content Disposition': 'attachment; filename=result.csv'
+        }
+    )
+    return response
+
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+
+    filename = f'{uuid.uuid4()}.csv'
+    df.to_csv(os.path.join('downloads', filename))
+
+    return render_template('04_index_download.html', filename=filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory('downloads', filename, download_name='result.csv')
+
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}')
+
+    return jsonify({'message': 'Succesfully written!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
